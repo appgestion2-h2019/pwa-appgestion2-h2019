aca86da5e05b3683cc680647b3883c270630b40b
Étienne Bouchard <etiennebouchard7@gmail.com>
2019-05-02 08:56:25 -0400

    Ajouts de sous-composantes pour les salles
    - Composante filtre
    - Composante popup-creation
    - Composante popup-modification
    - Composante popup-consultation

---
+++ src/app/salles/popup-modification/popup-modification.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-popup-modification',
+  templateUrl: './popup-modification.component.html',
+  styleUrls: ['./popup-modification.component.css']
+})
+export class PopupModificationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

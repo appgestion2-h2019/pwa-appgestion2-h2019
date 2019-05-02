216a80dddc9f7d0ecff7e244772cf80ad9624d87
carinecroteau <croteau.carine@carrefour.cegepvicto.ca>
2019-05-02 09:55:47 -0400

    Correction du fichier app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { ListeBlancheComponent } from './liste-blanche/liste-blanche.component';
 import { MessagerieComponent } from './messagerie/messagerie.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule} from '@angular/material';
-
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { CategoriesComponent} from './jeux/categories/categories.component';
+import { PictosComponent } from './pictos/pictos.component';
 import { GoogleComponent } from './authentification/google/google.component';
 import { FacebookComponent } from './authentification/facebook/facebook.component';
-
 import { SallesComponent } from './salles/salles.component';
 import { SalleComponent } from './salles/salle/salle.component';
 import { FiltreComponent } from './salles/filtre/filtre.component';
@@ -20,12 +21,22 @@ import { PopupModificationComponent } from './salles/popup-modification/popup-mo
 import { PopupConsultationComponent } from './salles/popup-consultation/popup-consultation.component';
 
 
-
 @NgModule({
   declarations: [
     AppComponent,
     ListeBlancheComponent,
-    MessagerieComponent
+    MessagerieComponent,
+    AuthentificationComponent,
+    PictosComponent,
+    GoogleComponent,
+    FacebookComponent,
+    SallesComponent,
+    SalleComponent,
+    CategoriesComponent,
+    FiltreComponent,
+    PopupCreationComponent,
+    PopupModificationComponent,
+    PopupConsultationComponent,
   ],
     imports: [
         BrowserModule,

a899ac349b6cdb0384982ca0fc4ee240cb9d99e5
dannick bedard <dannickbedard@gmail.com>
2019-05-02 09:25:33 -0400

    gestion des conflit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,10 @@ import { environment } from '../environments/environment';
 import { AuthentificationComponent } from './authentification/authentification.component';
 import { CategoriesComponent} from './jeux/categories/categories.component';
 import { PictosComponent } from './pictos/pictos.component';
-<<<<<<< HEAD
+
 import { GoogleComponent } from './authentification/google/google.component';
 import { FacebookComponent } from './authentification/facebook/facebook.component';
-=======
+
 import { SallesComponent } from './salles/salles.component';
 import { SalleComponent } from './salles/salle/salle.component';
 import { FiltreComponent } from './salles/filtre/filtre.component';
@@ -19,17 +19,17 @@ import { PopupCreationComponent } from './salles/popup-creation/popup-creation.c
 import { PopupModificationComponent } from './salles/popup-modification/popup-modification.component';
 import { PopupConsultationComponent } from './salles/popup-consultation/popup-consultation.component';
 
->>>>>>> 225fe02f9dc52ea9d0d5a2ce27d7f7fb496e02ec
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthentificationComponent,
     PictosComponent,
-<<<<<<< HEAD
+
     GoogleComponent,
     FacebookComponent,
-=======
+
     SallesComponent,
     SalleComponent,
     CategoriesComponent,
@@ -37,7 +37,7 @@ import { PopupConsultationComponent } from './salles/popup-consultation/popup-co
     PopupCreationComponent,
     PopupModificationComponent,
     PopupConsultationComponent,
->>>>>>> 225fe02f9dc52ea9d0d5a2ce27d7f7fb496e02ec
+
   ],
   imports: [
     BrowserModule,

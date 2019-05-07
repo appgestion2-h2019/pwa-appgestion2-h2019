e39a160a6f35222aae4497f8742bbb7fd0558395
carinecroteau <croteau.carine@carrefour.cegepvicto.ca>
2019-05-07 09:47:54 -0400

    ajout des routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -8,7 +9,7 @@ import { environment } from '../environments/environment';
 import { ListeBlancheComponent } from './liste-blanche/liste-blanche.component';
 import { MessagerieComponent } from './messagerie/messagerie.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatCardModule } from '@angular/material';
+import {MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatCardModule, MatToolbarModule, MatIconModule } from '@angular/material';
 import { AuthentificationComponent } from './authentification/authentification.component';
 import { CategoriesComponent} from './jeux/categories/categories.component';
 import { PictosComponent } from './pictos/pictos.component';
@@ -22,6 +23,20 @@ import { PopupModificationComponent } from './salles/popup-modification/popup-mo
 import { PopupConsultationComponent } from './salles/popup-consultation/popup-consultation.component';
 import { OptionsComponent } from './salles/options/options.component';
 import { SalleService } from './salles/salle/salle.service';
+
+const appRoutes: Routes = [
+  { path: 'accueil', component: AuthentificationComponent },
+  { path: 'connexion', component: AuthentificationComponent },
+  { path: 'salles', component: SallesComponent },
+  { path: 'messagerie', component: MessagerieComponent },
+  { path: 'salles/:id', component: SalleComponent }, // TODO Salle selon l'id
+  { path: 'jeux', component: SallesComponent }, // TODO Salle selon l'id
+  { path: '',
+    redirectTo: '/accueil',
+    pathMatch: 'full'
+  },
+  /*{ path: '**', component: PageNotFoundComponent }*/
+];
 
 @NgModule({
     declarations: [
@@ -42,6 +57,10 @@ import { SalleService } from './salles/salle/salle.service';
         OptionsComponent,
     ],
     imports: [
+        RouterModule.forRoot(
+          appRoutes,
+          { enableTracing: true } // <-- debugging purposes only
+        ),
         BrowserModule,
         AppRoutingModule,
         ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
@@ -51,6 +70,8 @@ import { SalleService } from './salles/salle/salle.service';
         MatSelectModule,
         MatButtonModule,
         MatCardModule,
+        MatToolbarModule,
+        MatIconModule,
         HttpClientModule,
     ],
     providers: [ SalleService ],

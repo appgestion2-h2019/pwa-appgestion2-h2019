e9c4c487aab9d95070e660d00939e463f4732f31
SachaLabbe <labbe.sacha@carrefour.cegepvicto.ca>
2019-05-09 08:37:48 -0400

    formulaire basic fait

--- src/app/formulaire-jeu/formulaire-jeu.component.ts
+++ src/app/formulaire-jeu/formulaire-jeu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Categorie } from '../categorie';
 
 @Component({
   selector: 'app-formulaire-jeu',
@@ -6,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulaire-jeu.component.css']
 })
 export class FormulaireJeuComponent implements OnInit {
-
+    categorie: Categorie = {
+        id: 1,
+        titre: 'Windstorm',
+        mot: [{nom: 'ffe'}, {niveau: 1}]
+    };
   constructor() { }
-
   ngOnInit() {
   }
 

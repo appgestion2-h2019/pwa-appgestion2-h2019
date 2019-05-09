95299e1733c8f220c32fab5cc4c6435006be4e0c
Étienne Bouchard <etiennebouchard7@gmail.com>
2019-05-09 10:12:37 -0400

    Ajout du MatTabsModule dans app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { environment } from '../environments/environment';
 import { ListeBlancheComponent } from './liste-blanche/liste-blanche.component';
 import { MessagerieComponent } from './messagerie/messagerie.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatCardModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatExpansionModule } from '@angular/material';
+import { MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatTabsModule, MatCardModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatExpansionModule } from '@angular/material';
 import { AuthentificationComponent } from './authentification/authentification.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { CategoriesComponent} from './jeux/categories/categories.component';
@@ -96,6 +96,7 @@ const appRoutes: Routes = [
         FormsModule,
         MatDialogModule,
         MatGridListModule,
+        MatTabsModule,
     ],
     exports : [
       PopupCreationComponent,

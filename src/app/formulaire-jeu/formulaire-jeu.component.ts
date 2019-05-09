4a9d10a847aba0c7b6841f797bf557b0d4cf37f7
SachaLabbe <labbe.sacha@carrefour.cegepvicto.ca>
2019-05-09 09:43:56 -0400

    Formulaire avec dropdown basés sur tableau dans .ts

--- src/app/formulaire-jeu/formulaire-jeu.component.ts
+++ src/app/formulaire-jeu/formulaire-jeu.component.ts
@@ -7,13 +7,25 @@ import { Categorie } from '../categorie';
   styleUrls: ['./formulaire-jeu.component.css']
 })
 export class FormulaireJeuComponent implements OnInit {
-    categorie: Categorie = {
-        id: 1,
-        titre: 'Windstorm',
-        mot: [{nom: 'ffe'}, {niveau: 1}]
-    };
-  constructor() { }
+    categorie: Categorie[] = [
+        {
+            id: 1,
+            titre: 'Cat1',
+            mot: [{nom: 'ffe1'}, {niveau: 1}]
+        },
+        {
+            id: 2,
+            titre: 'Cat2',
+            mot: [{nom: 'ffe2'}, {niveau: 1}]
+        },
+        {
+            id: 3,
+            titre: 'Cat3',
+            mot: [{nom: 'ffe3'}, {niveau: 1}]
+        }
+    ];
+
+    constructor() { }
   ngOnInit() {
   }
-
 }
